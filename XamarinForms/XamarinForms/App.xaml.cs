efb78ea16618d180b8db1e9500097a7f076f483c
Alex Kuang <akuang@microsoft.com>
2018-09-03 10:16:32 -0700

    XAM120 Exercise 1: Creating your first Xamarin.Forms application
    https://elearning.xamarin.com/forms/xam120/1-single-screen/exercise1/
    Note: UWP app project is not created because of using Visual Studio on
    Mac

---
+++ XamarinForms/XamarinForms/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace XamarinForms
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            //InitializeComponent();
+            //MainPage = new MainPage();
+
+            var view = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                    new Label {
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        Text = "Welcome to Xamarin Forms!"
+                    }
+                }
+            };
+
+            MainPage = new ContentPage
+            {
+                Content = view
+            };
+
+            Button button = new Button { Text = "Click Me" };
+            button.Clicked += async (s, e) =>
+            {
+                await MainPage.DisplayAlert("Alert", "You clicked me", "OK");
+            };
+
+            view.Children.Add(button);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
