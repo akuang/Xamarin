91509acda34d5b2634235719c2df67143492123e
Alex Kuang <akuang@microsoft.com>
2018-09-03 16:49:13 -0700

    Add permission helper NuGet package - Plugin.Permissions
    https://elearning.xamarin.com/forms/xam120/3-platform-specific/exercise3
    /7-add-permissions-package
    
    Using James Montemagno's Permissions Plugin NuGet package to abstract
    away as much of the Android-only implementation details as we can.

--- Phoneword/Phoneword.Android/MainActivity.cs
+++ Phoneword/Phoneword.Android/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.CurrentActivity;
+using Plugin.Permissions;
 
 namespace Phoneword.Droid
 {
@@ -18,8 +20,29 @@ namespace Phoneword.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+
+            // Connect the current activity plugin to the appropriate lifecycle 
+            // events in Android to always be aware of the current activity.
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+
+        /// <summary>
+        /// Enable the permission system in Android
+        /// </summary>
+        /// <param name="requestCode">Request code.</param>
+        /// <param name="permissions">Permissions.</param>
+        /// <param name="grantResults">Grant results.</param>
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            // When Android presents a permission request to the user at runtime, 
+            // this method is the callback the system uses to handle the user's 
+            // response. Instead of handling the request here directly, though, 
+            // we pass on any user response details to the permissions plugin.
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
