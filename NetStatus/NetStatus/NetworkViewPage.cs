47027f5f1fe2cebbd3efd2a62f5b017d8ed89b72
Alex Kuang <akuang@microsoft.com>
2018-09-04 22:51:54 -0700

    Display the network type
    https://elearning.xamarin.com/forms/xam150/1-obtain-the-devices-network-
    capabilities/exercise1/9-display-type

--- NetStatus/NetStatus/NetworkViewPage.cs
+++ NetStatus/NetStatus/NetworkViewPage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
 using Xamarin.Forms;
 
 
@@ -30,6 +31,32 @@ namespace NetStatus
         {
             base.OnAppearing();
             ConnectionDetails.Text += CrossConnectivity.Current.ConnectionTypes.First().ToString();
+
+            if (CrossConnectivity.Current == null)
+            {
+                return;
+            }
+
+            CrossConnectivity.Current.ConnectivityChanged += UpdateNetworkInfo;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            if (CrossConnectivity.Current != null)
+            {
+               //CrossConnectivity.Current.ConnectivityChanged -= UpdateNetworkInfo;
+            }
+        }
+
+        protected void UpdateNetworkInfo(object sender, ConnectivityChangedEventArgs e)
+        {
+            if (CrossConnectivity.Current?.ConnectionTypes != null)
+            {
+                var connectionType = CrossConnectivity.Current.ConnectionTypes.FirstOrDefault();
+                ConnectionDetails.Text = connectionType.ToString();
+            }
         }
     }
 }
