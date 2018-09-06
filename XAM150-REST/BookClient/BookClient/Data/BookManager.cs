14e2a3a403c20c89429478f4ed295801e8d79ef1
Alex Kuang <akuang@microsoft.com>
2018-09-06 00:15:05 -0700

    Perform REST operations
    https://elearning.xamarin.com/forms/xam150/3-consuming-rest-services-wit
    h-xamarin/exercise2/6-get-books

--- XAM150-REST/BookClient/BookClient/Data/BookManager.cs
+++ XAM150-REST/BookClient/BookClient/Data/BookManager.cs
@@ -1,34 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace BookClient.Data
 {
     public class BookManager
     {
-        public Task<IEnumerable<Book>> GetAll()
+        const string Url = "http://xam150.azurewebsites.net/api/books";
+        private string authorizationKey = string.Empty;
+
+        public async Task<IEnumerable<Book>> GetAll()
         {
             // TODO: use GET to retrieve books
-            throw new NotImplementedException();
+            List<Book> books = new List<Book>();
+
+            using (var httpClient = await GetHttpClient())
+            {
+                string json = await httpClient.GetStringAsync(Url);
+                if (!string.IsNullOrEmpty(json))
+                {
+                    books = JsonConvert.DeserializeObject<List<Book>>(json);
+                }
+            }
+
+            return books;
         }
 
-        public Task<Book> Add(string title, string author, string genre)
+        public async Task<Book> Add(string title, string author, string genre)
         {
             // TODO: use POST to add a book
-            throw new NotImplementedException();
+            Book book = new Book
+            {
+                Title = title,
+                ISBN = string.Empty,
+                Authors = new List<string> { author },
+                Genre = genre,
+                PublishDate = DateTime.Today
+            };
+
+            using (var httpClient = await GetHttpClient())
+            {
+                string json = JsonConvert.SerializeObject(book);
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync(Url, content);
+                var result = await response.Content.ReadAsStringAsync();
+                Book bookAdded = JsonConvert.DeserializeObject<Book>(result);
+
+                return bookAdded;
+            }
         }
 
-        public Task Update(Book book)
+        public async Task Update(Book book)
         {
             // TODO: use PUT to update a book
-            throw new NotImplementedException();
+            string json = JsonConvert.SerializeObject(book);
+            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            using(var httpClient = await GetHttpClient())
+            {
+                var result = await httpClient.PutAsync($"{Url}/{book.ISBN}", content);
+            }
         }
 
-        public Task Delete(string isbn)
+        public async Task Delete(string isbn)
         {
             // TODO: use DELETE to delete a book
-            throw new NotImplementedException();
+            using (var httpClient = await GetHttpClient())
+            {
+                var result = await httpClient.DeleteAsync($"{Url}/{isbn}");
+            }
+        }
+
+        private async Task<HttpClient> GetHttpClient()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            if (string.IsNullOrEmpty(authorizationKey))
+            {
+                authorizationKey = await httpClient.GetStringAsync($"{Url}/login");
+                authorizationKey = JsonConvert.DeserializeObject<string>(authorizationKey ?? string.Empty);
+            }
+
+            if (!string.IsNullOrEmpty(authorizationKey))
+            {
+                httpClient.DefaultRequestHeaders.Add("Authorization", authorizationKey);
+                httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+                return httpClient;
+            }
+
+            throw new Exception($"Unable to get authentication key from {Url}/login");
         }
     }
 }
