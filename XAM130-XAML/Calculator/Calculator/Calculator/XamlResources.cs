0b24749ed8953c8ae669cd200dd07ad8f37b5dc4
Alex Kuang <akuang@microsoft.com>
2018-09-07 22:55:29 -0700

    Use the {x:Static} markup extension
    https://elearning.xamarin.com/forms/xam130/3-explore-xaml/exercise3/3-XS
    tatic

---
+++ XAM130-XAML/Calculator/Calculator/Calculator/XamlResources.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public static class XamlResources
+    {
+        // Color.FromRgb(0xff, 0xa5, 0);
+        public static Color Orange => Color.FromHex("#FFA500"); 
+    }
+}
