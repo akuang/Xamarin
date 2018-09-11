afbfc0ba1e6af8103e6fa85c6a6efcabbd1f0830
Alex Kuang <akuang@microsoft.com>
2018-09-10 22:46:38 -0700

    Exercise 3: Access a SQLite database with SQLite.Net
    https://elearning.xamarin.com/forms/xam160/2-working-w-sqlite/exercise3/

---
+++ XAM160-SQLite/People/People/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+
+namespace People.Models
+{
+    [Table("Person")]
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Name { get; set; }
+    }
+}
